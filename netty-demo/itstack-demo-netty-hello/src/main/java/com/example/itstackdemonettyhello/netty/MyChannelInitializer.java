032ec83f1786ed9506522944b5c53e6dbf926c5d
Wu Xian Hu <xianhu.wu@ktbhealth.com>
2021-02-20 16:59:01 +0800

    fx

---
+++ netty-demo/itstack-demo-netty-hello/src/main/java/com/example/itstackdemonettyhello/netty/MyChannelInitializer.java
@@ -0,0 +1,18 @@
+package com.example.itstackdemonettyhello.netty;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        //处理http消息的编解码
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        //添加自定义的ChannelHandler
+        pipeline.addLast("httpServerHandler", new HttpServerHandler());
+
+    }
+}

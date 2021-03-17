2e6b3539ee196013a3ea1119b40e91a74d7e7f77
Wu Xian Hu <xianhu.wu@ktbhealth.com>
2021-03-17 15:26:21 +0800

    fx

---
+++ mybatisdemo/src/main/java/com/ktb/mapper/StudentMapper.java
@@ -0,0 +1,18 @@
+package com.ktb.mapper;
+
+import com.ktb.model.Student;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface StudentMapper {
+    // 查询学生
+    List<Student> getStudent();
+    Student getStudentByMap(HashMap map);
+
+    Student getStudentByIdAndName(Integer id, @Param("name") String name);
+
+
+}
+

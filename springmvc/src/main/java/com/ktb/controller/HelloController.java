a3e4e3ec60767b7bb3e5eb0d8ad6330a0cba7419
Wu Xian Hu <xianhu.wu@ktbhealth.com>
2021-03-18 14:59:06 +0800

    fx

--- springmvc/src/main/java/com/ktb/controller/HelloController.java
+++ springmvc/src/main/java/com/ktb/controller/HelloController.java
@@ -1,16 +1,27 @@
 package com.ktb.controller;
 
+import com.ktb.controller.model.User;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
-@RequestMapping("/yiidian")
 public class HelloController {
-    @RequestMapping("/hello.do")
-    public void hello(HttpServletResponse response) throws IOException {
-        response.getWriter().write("Hello-www.yiidian.com");
+
+    @RequestMapping(value = "/hello",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_XML_VALUE) //response header  Content-Type: application/xml
+    @ResponseBody
+    public  String handleJson() {
+        System.out.println("Got json request");
+        return "{ \"userName\": \"Joe\"}";
+    }
+
+    @RequestMapping(value = "/xml",produces = MediaType.APPLICATION_XML_VALUE)
+    public String handleXML() {
+        System.out.println("Got xml request");
+        return "success";
     }
 }

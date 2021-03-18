06a9e0b17081b33c87c9826b8d7f9cfed83b2b6e
Wu Xian Hu <xianhu.wu@ktbhealth.com>
2021-03-19 02:14:07 +0800

    fx

--- springmvc/src/main/java/com/ktb/controller/model/User.java
+++ springmvc/src/main/java/com/ktb/controller/model/User.java
@@ -1,22 +1,42 @@
 package com.ktb.controller.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
 public class User {
-    private Integer id;
-    private String  name;
 
-    public Integer getId() {
-        return id;
+    private String username;
+    private Integer age;
+
+
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="Asia/Shanghai")
+    private Date birthday;
+
+
+
+    public Date getBirthday() {
+        return birthday;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
---
+++ springmvc/src/main/webapp/WEB-INF/jsp/convert.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>一点教程网 - www.yiidian.com</title>
+</head>
+<body>
+<h2>自定义类型转换</h2>
+${user.birthday}
+</body>
+</html>

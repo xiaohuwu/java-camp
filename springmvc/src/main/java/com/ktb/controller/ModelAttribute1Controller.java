53dcb4434d49d22df3f18af2f133ee9c9b19bde2
xiaohuwu <1205150638@qq.com>
2021-04-03 17:10:19 +0800

    fx

---
+++ springmvc/src/main/java/com/ktb/controller/ModelAttribute1Controller.java
@@ -0,0 +1,25 @@
+package com.ktb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ModelAttribute1Controller {
+
+    // 使用@ModelAttribute注释的value属性，来指定model属性的名称,model属性对象就是方法的返回值
+    @ModelAttribute("loginname")
+    public String userModel1(
+            @RequestParam("loginname") String loginname) {
+        return loginname;
+    }
+
+    @RequestMapping(value = "/login1")
+    public String login1() {
+        return "result1";
+    }
+
+
+}

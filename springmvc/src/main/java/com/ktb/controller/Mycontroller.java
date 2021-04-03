4f3155ce6805d043a72db644fba3698fcab63776
xiaohuwu <1205150638@qq.com>
2021-04-03 17:10:19 +0800

    fx

--- springmvc/src/main/java/com/ktb/controller/Mycontroller.java
+++ springmvc/src/main/java/com/ktb/controller/Mycontroller.java
@@ -1,6 +1,7 @@
 package com.ktb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,5 +14,11 @@ public class Mycontroller {
         mv.setViewName("test");
         return mv;
     }
+
+
+
+
+
+
 
 }

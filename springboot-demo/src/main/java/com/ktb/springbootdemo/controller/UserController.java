e732529c5406635e0c9e37c3741061e9ba8ddb74
xiaohuwu <1205150638@qq.com>
2021-04-05 17:54:48 +0800

    fx

--- springboot-demo/src/main/java/com/ktb/springbootdemo/controller/UserController.java
+++ springboot-demo/src/main/java/com/ktb/springbootdemo/controller/UserController.java
@@ -1,16 +1,19 @@
 package com.ktb.springbootdemo.controller;
 
 import com.ktb.springbootdemo.model.UserEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @RequestMapping(value="/get",method= RequestMethod.GET)
     public Object getList(){
+        log.info("getList===");
         List<UserEntity> list= new ArrayList<UserEntity>();
         UserEntity u1 = new UserEntity(null, "shanghai");
         list.add(u1);

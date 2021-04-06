b3e7f99b147dc4aac9d2977fdb97c616c39a3905
xiaohuwu <1205150638@qq.com>
2021-04-07 01:41:53 +0800

    fx

---
+++ spring-boot-redis/src/main/java/com/ktb/springbootredis/SpringBootRedisApplication.java
@@ -0,0 +1,69 @@
+package com.ktb.springbootredis;
+
+import com.ktb.springbootredis.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Method;
+
+@RestController
+@EnableCaching
+@SpringBootApplication
+public class SpringBootRedisApplication {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisApplication.class, args);
+    }
+
+    @PostMapping("/getUser")
+    @Cacheable(value = "user", key ="'id:'+#user.id")
+    public User getUser(@RequestBody User user) {
+        System.out.println("用户对象缓存不存在，返回一个新的用户对象。");
+        return user;
+    }
+
+    @PostMapping("/updateUser")
+    @CachePut(value = "user", key ="'id:'+#user.id")
+    public User updateUser(@RequestBody User user) {
+        System.out.println("用户对象缓存不存在，返回一个新的用户对象。");
+        return user;
+    }
+
+    @GetMapping("/delUser")
+    @CacheEvict(value = "user", key ="'id:'+ #p0")
+    public String  delUser(Integer id) {
+        System.out.println("用户对象缓存不存在，返回一个新的用户对象。");
+        return "ok";
+    }
+
+
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+}

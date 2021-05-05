3c4a00d58458f7a6d7f9eafe7371991d3d74f690
xiaohuwu <1205150638@qq.com>
2021-05-05 10:51:51 +0800

    fx

---
+++ springcloud/service-miya/src/test/java/com/example/servicemiya/ServiceMiyaApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.servicemiya;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ServiceMiyaApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

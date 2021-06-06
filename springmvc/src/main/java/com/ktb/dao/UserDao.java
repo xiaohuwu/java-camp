95bb4fd441b156f9d8152548d66381942e087067
xiaohuwu <1205150638@qq.com>
2021-06-07 00:31:07 +0800

    fx

--- springmvc/src/main/java/com/ktb/dao/UserDao.java
+++ springmvc/src/main/java/com/ktb/dao/UserDao.java
@@ -1,10 +1,14 @@
 package com.ktb.dao;
 
 import com.ktb.model.MybatisUser;
+import com.ktb.model.User;
+import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
 
 
+@Mapper
 public interface UserDao {
     List<MybatisUser> listAllUsers();
 
@@ -15,4 +19,6 @@ public interface UserDao {
     void updateUser(MybatisUser user);
 
     void deleteUser(Integer id);
+
+    List<User> getAllUser();
 }

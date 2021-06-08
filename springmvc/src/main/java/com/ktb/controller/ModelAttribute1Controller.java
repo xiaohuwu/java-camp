2b30a85dc8fe3f1e049bb1e5ca1ca8012e9fca57
xiaohuwu <1205150638@qq.com>
2021-06-08 10:29:03 +0800

    fx

--- springmvc/src/main/java/com/ktb/controller/ModelAttribute1Controller.java
+++ springmvc/src/main/java/com/ktb/controller/ModelAttribute1Controller.java
@@ -1,6 +1,7 @@
 package com.ktb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ModelAttribute1Controller {
 
-    // 使用@ModelAttribute注释的value属性，来指定model属性的名称,model属性对象就是方法的返回值
+    // 使用@ModelAttribute注释的value属性，来指定model属性的名称,model属性值就是方法的返回值
     @ModelAttribute("loginname")
     public String userModel1(
             @RequestParam("loginname") String loginname) {
@@ -17,11 +18,19 @@ public class ModelAttribute1Controller {
     }
 
     @RequestMapping(value = "/login1")
-    public String login1() {
+    public String login1(ModelMap modelMap) {
+        Object loginname = modelMap.get("loginname");
+        System.out.println("loginname:"+ loginname);
         return "result1";
     }
 
 
+    @RequestMapping(value = "/login2")
+    public String login2(ModelMap modelMap) {
+        Object loginname = modelMap.get("loginname");
+        System.out.println("loginname22:"+ loginname);
+        return "result1";
+    }
 
 
 }

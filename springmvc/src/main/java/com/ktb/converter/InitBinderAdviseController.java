9b11790c226dab0b75f62069e635c7b3aa05dca0
xiaohuwu <1205150638@qq.com>
2021-06-09 11:17:36 +0800

    fx

--- springmvc/src/main/java/com/ktb/converter/InitBinderAdviseController.java
+++ springmvc/src/main/java/com/ktb/converter/InitBinderAdviseController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.InitBinder;
 
 import java.util.Date;
 
+/**
+ * 和 message converter 类似 全局的数据类型 转换器
+ * @InitBinder 也可以定义在controller内部
+ */
+
 @ControllerAdvice
 public class InitBinderAdviseController {
 
-//    @InitBinder
-//    public void dateTypeBinder(WebDataBinder webDataBinder){
-//        //往数据绑定器中添加一个DateFormatter日期转化器。
-//        webDataBinder.registerCustomEditor(Date.class, new DateEditor());
-//    }
+    @InitBinder
+    public void dateTypeBinder(WebDataBinder webDataBinder){
+        //往数据绑定器中添加一个DateFormatter日期转化器。
+        webDataBinder.registerCustomEditor(Date.class, new DateEditor());
+    }
 
 }

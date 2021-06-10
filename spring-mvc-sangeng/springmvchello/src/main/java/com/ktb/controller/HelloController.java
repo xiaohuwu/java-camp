71a3d674204941996eeb4da966cf1ed9a0e741c5
xiaohuwu <1205150638@qq.com>
2021-06-10 20:32:37 +0800

    fx

---
+++ spring-mvc-sangeng/springmvchello/src/main/java/com/ktb/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ktb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public String helloWorld(){
+        return "hello.jsp";
+    }
+}

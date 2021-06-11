5027468b1ab24e6e37698f95c93abcb804ac34bf
xiaohuwu <1205150638@qq.com>
2021-06-11 19:28:22 +0800

    fx

--- spring-mvc-sangeng/springmvchello/src/main/java/com/ktb/controller/HelloController.java
+++ spring-mvc-sangeng/springmvchello/src/main/java/com/ktb/controller/HelloController.java
@@ -3,13 +3,28 @@ package com.ktb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "/{path}")
-    public String helloWorld(@PathVariable("path") String path) {
-        return path;
+    @RequestMapping(value = "/hello")
+    public ModelAndView helloWorld(@PathVariable(value = "path",required = false) String path) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("msg","HelloSpringMVC!");
+        mv.setViewName("hello"); //: /WEB-INF/jsp/hello.jsp
+        return mv;
     }
+
+
+
+    @RequestMapping(value = "/test")
+    @ResponseBody
+    public String test(@PathVariable(value = "path",required = false) String path) {
+        return "test";
+    }
+
+
 }

0ed2c12313a49e491b01b09859fafb099d2063f2
xiaohuwu <1205150638@qq.com>
2021-06-16 22:11:06 +0800

    fx

---
+++ gitchat-demo/springboot-mybatis/src/main/java/com/ktb/springbootmybatis/SpringbootMybatisApplication.java
@@ -0,0 +1,33 @@
+package com.ktb.springbootmybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.VendorDatabaseIdProvider;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.ktb.springbootmybatis.dao")
+public class SpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisApplication.class, args);
+    }
+
+    
+    @Bean
+    public SqlSessionFactoryBean getSqlSessionFactory(DataSource dataSource) throws IOException {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(pathMatchingResourcePatternResolver.getResources("classpath*:mapper/*.xml"));
+        return sqlSessionFactoryBean;
+    }
+
+}

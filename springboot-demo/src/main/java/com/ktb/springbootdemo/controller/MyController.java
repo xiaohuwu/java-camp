041a3ff2b902a6fb4f4325f70952011aa8cd9808
xiaohuwu <1205150638@qq.com>
2021-06-18 10:32:45 +0800

    fx

--- springboot-demo/src/main/java/com/ktb/springbootdemo/controller/MyController.java
+++ springboot-demo/src/main/java/com/ktb/springbootdemo/controller/MyController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Controller
 public class MyController {
-    @GetMapping("/")
+   // @GetMapping("/")
     public String mainView() {
         return "main";
     }
--- springboot-demo/src/test/java/com/ktb/springbootdemo/Test.java
+++ springboot-demo/src/test/java/com/ktb/springbootdemo/Test.java
@@ -1,5 +1,17 @@
 package com.ktb.springbootdemo;
 
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import net.minidev.json.JSONUtil;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import org.junit.jupiter.api.*;
+
 public class Test {
     private String name;
 
@@ -11,8 +23,57 @@ public class Test {
         this.name = name;
     }
 
-    public static void main(String[] args) {
-        System.out.println(BeanUtils.convertToFieldName(Test::getName));
+    /**
+     * * 5. 方法引用使用的要求：要求接口中的抽象方法的形参列表和返回值类型与方法引用的方法的
+     *  *    形参列表和返回值类型相同！（针对于情况1和情况2）
+     */
+    @org.junit.jupiter.api.Test
+    public void test1(){
+        Consumer<String> test = new Consumer<String>(){
+
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        };
+        test.accept("6666");
+        Consumer<String> con1 = str -> System.out.println(str);
+        con1.accept("北京");
+
+        System.out.println("*******************");
+
+        PrintStream ps = System.out;
+        Consumer<String> con2 = System.out::println;
+        con2.accept("beijing");
 
     }
+
+
+    //Supplier中的T get()
+    //Employee中的String getName()
+    @org.junit.jupiter.api.Test
+    public void test2() {
+        Employee emp = new Employee(1001, "Tom", 23, 5600);
+
+        Supplier<String> sup1 = () -> emp.getName();
+        System.out.println(sup1.get());
+
+        System.out.println("*******************");
+        Supplier<String> sup2 = emp::getName;
+        System.out.println(sup2.get());
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void test3() {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aaa");
+        strings.add("bbb");
+        strings.add("ccc");
+        Consumer consumer = System.out::println;
+        strings.forEach(consumer);
+    }
+
+
+
 }

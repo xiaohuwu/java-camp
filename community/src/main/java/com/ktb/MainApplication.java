b2599c9313274be47bc7eb4b95c23ff084fb1b0b
xiaohuwu <1205150638@qq.com>
2021-08-13 10:01:16 +0800

    fx

--- community/src/main/java/com/ktb/MainApplication.java
+++ community/src/main/java/com/ktb/MainApplication.java
@@ -1,16 +1,17 @@
 package com.ktb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@MapperScan("com.ktb.dao")
 public class MainApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
         String[] beanDefinitionNames = run.getBeanDefinitionNames();
         System.out.printf("beanDefinitionNames==",beanDefinitionNames);
-
     }
 }

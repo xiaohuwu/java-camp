49f1d63784157491057682fd9968482375150974
xiaohuwu <1205150638@qq.com>
2021-09-06 17:29:53 +0800

    fx

--- about-spring/spring-sangeng/src/main/java/com/ktb/juejin/importexample/TavernConfigurationTest.java
+++ about-spring/spring-sangeng/src/main/java/com/ktb/juejin/importexample/TavernConfigurationTest.java
@@ -1,15 +1,21 @@
-package com.ktb;
+package com.ktb.juejin.importexample;
 
 
 import com.ktb.model.Bartender;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.util.Map;
 import java.util.stream.Stream;
 
+//@Import 注解 示例
 public class TavernConfigurationTest {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(TavernConfiguration.class);
+//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(TavernConfiguration.class);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.getEnvironment().setActiveProfiles("dev");
+        ctx.register(TavernConfiguration.class);
+        ctx.refresh();
         String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
         Stream.of(beanDefinitionNames).forEach(System.out::println);
         System.out.println("--------------------------");
--- about-spring/spring-sangeng/src/main/java/com/ktb/juejin/importexample/config/BartenderConfiguration.java
+++ about-spring/spring-sangeng/src/main/java/com/ktb/juejin/importexample/config/BartenderConfiguration.java
@@ -1,16 +1,17 @@
-package com.ktb.config;
+package com.ktb.juejin.importexample.config;
 
 import com.ktb.model.Bartender;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class BartenderConfiguration {
     @Bean
     public Bartender zhangxiaosan() {
         return new Bartender("张小三");
     }
-
     @Bean
     public Bartender zhangdasan() {
         return new Bartender("张大三");

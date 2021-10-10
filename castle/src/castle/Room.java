c9f67506b821882a14ab06ec5d97b983b33f79ef
xiaohuwu <1205150638@qq.com>
2021-10-10 17:04:15 +0800

    解耦合第一步

--- castle/src/castle/Room.java
+++ castle/src/castle/Room.java
@@ -24,6 +24,21 @@ public class Room {
             westExit = west;
     }
 
+
+    public void showPromt() {
+        System.out.println("你在:" + this.toString());
+        System.out.print("出口有: ");
+        if (this.northExit != null)
+            System.out.print("north ");
+        if (this.eastExit != null)
+            System.out.print("east ");
+        if (this.southExit != null)
+            System.out.print("south ");
+        if (this.westExit != null)
+            System.out.print("west ");
+        System.out.println();
+    }
+
     @Override
     public String toString()
     {

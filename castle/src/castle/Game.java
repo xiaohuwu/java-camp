00c72b10ab6ba61facaa5b8d153bca1d7275d0bd
xiaohuwu <1205150638@qq.com>
2021-10-10 21:12:09 +0800

    fx

--- castle/src/castle/Game.java
+++ castle/src/castle/Game.java
@@ -1,22 +1,22 @@
 package castle;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Game {
     private Room currentRoom;
     private Integer currentIndex = 0;
-    ArrayList<Room> rooms;
-        
-    public Game() 
-    {
+    public static HashMap<String, Handler> cmdHash = new HashMap<String, Handler>();
+    ArrayList<Room> rooms = new ArrayList<>();
+
+    public Game() {
         createRooms();
     }
 
-    private void createRooms()
-    {
+    private void createRooms() {
         Room outside, lobby, pub, study, bedroom;
-      
+
         //	制造房间
         outside = new Room("城堡外");
         lobby = new Room("大堂");
@@ -30,13 +30,16 @@ public class Game {
         rooms.add(study);
         rooms.add(bedroom);
 
-        outside.setRoom("west",lobby);
-        outside.setRoom("north",pub);
+        outside.setRoom("west", lobby);
+        outside.setRoom("north", pub);
 
-        lobby.setRoom("north",study);
-        pub.setRoom("north",study);
-        bedroom.setRoom("north",study);
+        lobby.setRoom("north", study);
+        pub.setRoom("north", study);
+        bedroom.setRoom("north", study);
 
+        cmdHash.put("bye", new ByeHandler(this));
+        cmdHash.put("go", new Gohandler(this));
+        cmdHash.put("help", new HelpHandler(this));
 
 
         //	初始化房间的出口
@@ -60,54 +63,62 @@ public class Game {
 
     // 以下为用户命令
 
-    private void printHelp() 
-    {
+    private void printHelp() {
         System.out.print("迷路了吗？你可以做的命令有：go bye help");
         System.out.println("如：\tgo east");
     }
 
-    private void goRoom(String direction) 
-    {   Room nextRoom = null;
-        if("all_rotation".equals(direction)){
+    public void goRoom(String direction) {
+        Room nextRoom = null;
+        if ("all_rotation".equals(direction)) {
             nextRoom = getRotationRoom();
-        }else {
+        } else {
             nextRoom = currentRoom.getNextRoom(direction);
         }
         if (nextRoom == null) {
             System.out.println("那里没有门！");
-        }else {
+        } else {
             currentRoom = nextRoom;
             currentRoom.showPromt();
         }
     }
 
-    public Room getRotationRoom(){
+    public Room getRotationRoom() {
         int nextInt = currentIndex % rooms.size();
         currentIndex++;
         return rooms.get(nextInt);
     }
 
 
-
     public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		Game game = new Game();
-		game.printWelcome();
-		// 这一部分 还是硬编码
-        while ( true ) {
-        		String line = in.nextLine();
-        		String[] words = line.split(" ");
-        		if ( words[0].equals("help") ) {
-        			game.printHelp();
-        		} else if (words[0].equals("go") ) {
-        			game.goRoom(words[1]);
-        		} else if ( words[0].equals("bye") ) {
-        			break;
-        		}
-        }
-        
+        play();
         System.out.println("感谢您的光临。再见！");
+
+    }
+
+    private static void play() {
+        Scanner in = new Scanner(System.in);
+        Game game = new Game();
+        game.printWelcome();
+        // 这一部分 还是硬编码
+        while (true) {
+            String line = in.nextLine();
+            String[] words = line.split(" ");
+            Handler handler = cmdHash.get(words[0]);
+            if (handler.exit()) {
+                break;
+            } else {
+                handler.Cmd(words[1]);
+            }
+//        		if ( words[0].equals("help") ) {
+//        			game.printHelp();
+//        		} else if (words[0].equals("go") ) {
+//        			game.goRoom(words[1]);
+//        		} else if ( words[0].equals("bye") ) {
+//        			break;
+//        		}
+        }
         in.close();
-	}
+    }
 
 }

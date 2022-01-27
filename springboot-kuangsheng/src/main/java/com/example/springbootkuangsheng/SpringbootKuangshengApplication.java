13eed120f3b2af1c7c7bee96e2a54b93f9094709
xiaohuwu <1205150638@qq.com>
2022-01-27 14:48:06 +0800

    fx

--- springboot-kuangsheng/src/main/java/com/example/springbootkuangsheng/SpringbootKuangshengApplication.java
+++ springboot-kuangsheng/src/main/java/com/example/springbootkuangsheng/SpringbootKuangshengApplication.java
@@ -2,8 +2,11 @@ package com.example.springbootkuangsheng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class SpringbootKuangshengApplication {
@@ -12,6 +15,17 @@ public class SpringbootKuangshengApplication {
         SpringApplication.run(SpringbootKuangshengApplication.class, args);
     }
 
+
+
+//    @Bean
+//    public ServletRegistrationBean dispatcherRegistration() {
+//        return new ServletRegistrationBean(dispatcherServlet());
+//    }
+//
+//    @Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+//    public DispatcherServlet dispatcherServlet() {
+//        return new LoggableDispatcherServlet();
+//    }
 
 //    @Bean
 //    public CommonsRequestLoggingFilter logFilter() {
---
+++ springboot-kuangsheng/src/main/java/com/example/springbootkuangsheng/WebLogAspect.java
@@ -0,0 +1,98 @@
+package com.example.springbootkuangsheng;
+
+import com.google.gson.Gson;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.*;
+
+@Aspect
+@Component
+@Order(1)
+public class WebLogAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebLogAspect.class);
+
+    @Pointcut("execution(public *  com.example.springbootkuangsheng.controller.*.*(..))")
+    public void webLog() {
+    }
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        LOGGER.info("URL : " + request.getRequestURL().toString());
+        LOGGER.info("HTTP_METHOD : " + request.getMethod() + "  IP : " + request.getRemoteAddr());
+        LOGGER.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        Signature signature = joinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) signature;
+        Method method = methodSignature.getMethod();
+        Object parameter = getParameter(method, joinPoint.getArgs());
+        LOGGER.info("ARGS : " + new Gson().toJson(parameter));
+    }
+
+    @AfterReturning(value = "webLog()", returning = "ret")
+    public void doAfterReturning(Object ret) throws Throwable {
+    }
+
+//    @Around("webLog()")
+//    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+//        long startTime = System.currentTimeMillis();
+//        //获取当前请求对象
+//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+//        HttpServletRequest request = attributes.getRequest();
+//        //记录请求信息
+//
+//        Object result = joinPoint.proceed();
+//        Signature signature = joinPoint.getSignature();
+//        MethodSignature methodSignature = (MethodSignature) signature;
+//        Method method = methodSignature.getMethod();
+//        long endTime = System.currentTimeMillis();
+//        return result;
+//    }
+
+    /**
+     * 根据方法和传入的参数获取请求参数
+     */
+    private Object getParameter(Method method, Object[] args) {
+        List<Object> argList = new ArrayList<>();
+        Parameter[] parameters = method.getParameters();
+        for (int i = 0; i < parameters.length; i++) {
+            //将RequestBody注解修饰的参数作为请求参数
+            RequestBody requestBody = parameters[i].getAnnotation(RequestBody.class);
+            if (requestBody != null) {
+                argList.add(args[i]);
+            }
+            //将RequestParam注解修饰的参数作为请求参数
+            RequestParam requestParam = parameters[i].getAnnotation(RequestParam.class);
+            if (requestParam != null) {
+                Map<String, Object> map = new HashMap<>();
+                String key = parameters[i].getName();
+                if (!StringUtils.isEmpty(requestParam.value())) {
+                    key = requestParam.value();
+                }
+                map.put(key, args[i]);
+                argList.add(map);
+            }
+        }
+        if (argList.size() == 0) {
+            return null;
+        } else if (argList.size() == 1) {
+            return argList.get(0);
+        } else {
+            return argList;
+        }
+    }
+}

5ee6c9a9bb311984c370a8c18685d0bcdf4992ac
xiaohuwu <1205150638@qq.com>
2022-02-07 16:05:52 +0800

    fx

---
+++ nowcoder/src/com/ktb/leetcode/otherlinklist/Solution.java
@@ -0,0 +1,47 @@
+package com.ktb.leetcode.otherlinklist;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+//        ListNode three = new ListNode(3);
+//        ListNode four = new ListNode(4);
+//        ListNode five = new ListNode(5);
+        one.setNext(two);
+//        two.setNext(three);
+//        three.setNext(four);
+//        four.setNext(five);
+        printNode(one);
+        ListNode listNode = reverseList(one);
+        printNode(listNode);
+    }
+
+    /**
+     * 反转链表 差点我就写出来了，不知道怎么访问前一个节点
+     * 反转链表 本质需要当前节点访问到前一个节点
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        while (current.next != null) {
+            ListNode temp = current.next;
+            current.next = null;
+            current = temp;
+        }
+        return current;
+    }
+
+
+    private static void printNode(ListNode one) {
+        ListNode current = one;
+        while (current != null) {
+            System.out.print(current.getVal() + "->");
+            current = current.getNext();
+        }
+        System.out.println();
+    }
+
+
+}

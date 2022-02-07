913461395f8d42d126278238ea8cca1fa0a4ffd2
xiaohuwu <1205150638@qq.com>
2022-02-07 16:22:53 +0800

    fx

--- nowcoder/src/com/ktb/leetcode/otherlinklist/Solution.java
+++ nowcoder/src/com/ktb/leetcode/otherlinklist/Solution.java
@@ -5,13 +5,13 @@ public class Solution {
     public static void main(String[] args) {
         ListNode one = new ListNode(1);
         ListNode two = new ListNode(2);
-//        ListNode three = new ListNode(3);
-//        ListNode four = new ListNode(4);
-//        ListNode five = new ListNode(5);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
         one.setNext(two);
-//        two.setNext(three);
-//        three.setNext(four);
-//        four.setNext(five);
+        two.setNext(three);
+        three.setNext(four);
+        four.setNext(five);
         printNode(one);
         ListNode listNode = reverseList(one);
         printNode(listNode);
@@ -19,18 +19,21 @@ public class Solution {
 
     /**
      * 反转链表 差点我就写出来了，不知道怎么访问前一个节点
-     * 反转链表 本质需要当前节点访问到前一个节点
+     * 反转链表 本质需要当前节点指向前一个节点
+     *
      * @param head
      * @return
      */
     public static ListNode reverseList(ListNode head) {
         ListNode current = head;
-        while (current.next != null) {
+        ListNode pre = null;
+        while (current != null) {
             ListNode temp = current.next;
-            current.next = null;
+            current.next = pre;
+            pre = current;
             current = temp;
         }
-        return current;
+        return pre;
     }
 
 

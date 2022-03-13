e4f06633269d4dbe6a0611dcf115894cbca99064
xiaohuwu <1205150638@qq.com>
2022-03-14 00:27:58 +0800

    fx

--- mldnboot/mldnboot-base/src/test/java/com/mldn/mldnbootbase/MldnbootBaseApplicationTests.java
+++ mldnboot/mldnboot-base/src/test/java/com/mldn/mldnbootbase/MldnbootBaseApplicationTests.java
@@ -1,13 +1,25 @@
 package com.mldn.mldnbootbase;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@SpringBootTest
-class MldnbootBaseApplicationTests {
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest(classes = MldnbootBaseApplication.class)		// 定义要测试的SpringBoot类
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MldnbootBaseApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws SQLException {
+        System.out.println("dataSource.getConnection() = " + dataSource.getConnection());
     }
 
 }

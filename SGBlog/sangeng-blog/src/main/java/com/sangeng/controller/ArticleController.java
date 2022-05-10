ab38e85f48a052eac4168a2ad08daed9bd4a1cc3
xiaohuwu <1205150638@qq.com>
2022-05-10 23:48:06 +0800

    fx

---
+++ SGBlog/sangeng-blog/src/main/java/com/sangeng/controller/ArticleController.java
@@ -0,0 +1,35 @@
+package com.sangeng.controller;
+
+import com.sangeng.entity.Article;
+import com.sangeng.entity.ResponseResult;
+import com.sangeng.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping("/list")
+    public List<Article> test(){
+        return articleService.list();
+    }
+
+
+    @GetMapping("/hotArticleList")
+    public ResponseResult hotArticleList(){
+
+        ResponseResult result =  articleService.hotArticleList();
+        return result;
+    }
+
+
+}
---
+++ SGBlog/sangeng-framework/src/main/java/com/sangeng/service/ArticleService.java
@@ -0,0 +1,17 @@
+package com.sangeng.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.sangeng.entity.Article;
+import com.sangeng.entity.ResponseResult;
+
+/**
+ * 文章表(Article)表服务接口
+ *
+ * @author 66
+ * @since 2022-05-10 23:17:01
+ */
+public interface ArticleService extends IService<Article> {
+
+    ResponseResult hotArticleList();
+}
+

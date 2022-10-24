1d7f5cab98bb6a37f70ce99130a3f6449e16f7aa
xioahuge <xioahuge@xioahugedeMacBook-Pro.local>
2022-10-24 23:12:46 +0800

    fx

---
+++ springmvc/spring-mvc-sangeng/springmvchello/src/main/java/com/ktb/controller/ReviewController.java
@@ -0,0 +1,15 @@
+package com.ktb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ReviewController {
+
+    @RequestMapping("/testPath")
+    public String test() {
+        return "success";
+    }
+
+}

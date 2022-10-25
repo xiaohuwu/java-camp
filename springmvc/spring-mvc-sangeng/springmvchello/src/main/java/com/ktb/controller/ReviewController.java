6bf8f9ac6e7001d055031154e1bf0655f84f0c8d
xiaohu <1205150638@qq.com>
2022-10-25 16:00:24 +0800

    fx

--- springmvc/spring-mvc-sangeng/springmvchello/src/main/java/com/ktb/controller/ReviewController.java
+++ springmvc/spring-mvc-sangeng/springmvchello/src/main/java/com/ktb/controller/ReviewController.java
@@ -1,10 +1,10 @@
 package com.ktb.controller;
+
 import com.ktb.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -13,23 +13,24 @@ public class ReviewController {
 
     @RequestMapping(value = "/testPath", params = "testParam=123", headers = "testHeader=123", method = RequestMethod.GET)
     public String test() {
-        return "success";
+        return "/success.jsp";
     }
 
     @RequestMapping(value = "/testPath1", consumes = "application/json")
     public String test1() {
-        return "success";
+        return "/success.jsp";
     }
 
     @RequestMapping(value = "/queryId/{id}", consumes = "application/json")
     public String test2(@PathVariable("id") String id) {
         System.out.println("id = " + id);
-        return "success";
+        return "/success.jsp";
     }
 
 
     /**
      * 如果需要使用**@RequestBody**来获取请求体中Json并且进行转换，要求请求头 Content-Type 的值要为： application/json
+     *
      * @param user
      * @return
      */
@@ -41,7 +42,8 @@ public class ReviewController {
 
 
     /**
-     *http://localhost:8080/userQuery?name=geg&age=24
+     * http://localhost:8080/userQuery?name=geg&age=24
+     *
      * @param user
      * @return
      */
@@ -49,6 +51,39 @@ public class ReviewController {
     public String test4(User user) {
         System.out.println("id = " + user);
         return user.toString();
+    }
+
+
+    /**
+     * success.jsp 如果不通过controler访问 需要直接放在 webapp下
+     *
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "/redirect")
+    public String redirect(User user) {
+        System.out.println("id = " + user);
+        return "redirect:/success.jsp";
+    }
+
+
+    @RequestMapping(value = "/getHeader", method = RequestMethod.GET)
+    @ResponseBody
+    public String getHeader(@RequestHeader(value = "device-type") String deviceType) {
+        System.out.println("deviceType = " + deviceType);
+        return "redirect:/success.jsp";
+    }
+
+
+    @RequestMapping(value = "/login_new", method = RequestMethod.GET)
+    public String login_new() {
+        return "login_new";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(String userName, HttpSession session) {
+        session.setAttribute("userName", userName);
+        return "redirect:/success.jsp";
     }
 
 }

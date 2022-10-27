86c59e5ec53969ec750d1c78a35863ea65609502
xiaohu <1205150638@qq.com>
2022-10-27 16:54:15 +0800

    fx

--- mybatis/mybatisdemo/src/main/java/com/ktb/model/Order.java
+++ mybatis/mybatisdemo/src/main/java/com/ktb/model/Order.java
@@ -9,7 +9,16 @@ public class Order {
     private Integer price;
     private String remark;
     private Integer userId;
+    private User user;
 
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public Integer getId() {
         return id;

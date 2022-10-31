95a7804e7f054ff76165d8cad1092f1bbb9444a0
xiaohu <1205150638@qq.com>
2022-10-31 22:18:46 +0800

    fx

--- Java8/Java8/README.md
+++ Java8/Java8/README.md
@@ -1,3 +1,4 @@
-https://www.benchresources.net 
-
+https://www.benchresources.net
+https://mp.weixin.qq.com/s/ykHkf0OuuyMsJhy83iVI4w
+https://juejin.cn/post/6844903830254010381
 []: # Title: Java 8
--- project/newbee/src/test/java/com/ktb/newbee/NewbeeApplicationTests.java
+++ project/newbee/src/test/java/com/ktb/newbee/NewbeeApplicationTests.java
@@ -1,16 +1,59 @@
 package com.ktb.newbee;
 
-import cn.hutool.crypto.SecureUtil;
+import com.ktb.newbee.dao.UserDao;
+import com.ktb.newbee.entity.Employee;
+import com.ktb.newbee.entity.User;
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.core.diff.Diff;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.javers.core.diff.ListCompareAlgorithm.LEVENSHTEIN_DISTANCE;
 
 @SpringBootTest
 class NewbeeApplicationTests {
 
-	@Test
-	void contextLoads() {
-		String md5Str = SecureUtil.md5("123456");
-		System.out.println(md5Str);
-	}
+    @Autowired
+    UserDao userDao;
+
+    /**
+     * 测试javers 能快速比较两个对象的差异
+     */
+    @Test
+    void contextLoads() {
+//		String md5Str = SecureUtil.md5("123456");
+//		System.out.println(md5Str);
+        User user = userDao.selectById(1);
+        User user2 = new User();
+        BeanUtils.copyProperties(user, user2);
+        user2.setNickName("xiaohuge123");
+
+        Javers javers = JaversBuilder.javers().build();
+        Diff diff = javers.compare(user, user2);
+        System.out.println(javers.getJsonConverter().toJson(diff));
+
+    }
+
+    @Test
+    public void  test1(){
+        Javers javers = JaversBuilder.javers()
+                .withListCompareAlgorithm(LEVENSHTEIN_DISTANCE)
+                .build();
+
+        Employee frodoOld = new Employee()
+                .setName("Frodo")
+                .setSalary(10000);
+
+        Employee frodonew = new Employee()
+                .setName("Frodo")
+                .setSalary(2000);
+
+        //when
+        Diff diff = javers.compare(frodoOld, frodonew);
+        System.out.println(diff.prettyPrint());
+    }
 
 }

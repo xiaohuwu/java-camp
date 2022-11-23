b6e2be8a49e592f7b638b708c988aecc9190e058
xiaohu <1205150638@qq.com>
2022-11-23 22:13:50 +0800

    first commit

---
+++ nowcoder/src/com/ktb/sort/SelectSort.java
@@ -0,0 +1,30 @@
+package com.ktb.sort;
+
+import java.util.Arrays;
+
+//doc/select_sort
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] array = {6, 7, 8, 9, 4, 5, 3, 2, 1};
+        //选择排序
+        array = select_sort(array);
+        System.out.println("选择排序");
+        System.out.println(Arrays.toString(array));
+
+    }
+
+    private static int[] select_sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int k = i + 1; k < array.length; k++) {
+                if (array[k] <  array[i]) {
+                    int temp = array[i];
+                    array[i] = array[k];
+                    array[k] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+}

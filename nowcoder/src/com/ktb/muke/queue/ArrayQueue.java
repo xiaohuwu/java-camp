0f08dafccd0925f9a0e526eaa21dd74cc494e38b
xiaohu <1205150638@qq.com>
2022-11-28 22:34:32 +0800

    first commit

--- nowcoder/src/com/ktb/muke/queue/ArrayQueue.java
+++ nowcoder/src/com/ktb/muke/queue/ArrayQueue.java
@@ -49,7 +49,6 @@ public class ArrayQueue<T> {
 
 
     public static void main(String[] args){
-
         ArrayQueue<Integer> queue = new ArrayQueue<>();
         for(int i = 0 ; i < 10 ; i ++){
             queue.enqueue(i);

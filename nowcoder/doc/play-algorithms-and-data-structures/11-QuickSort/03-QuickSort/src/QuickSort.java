1d338fb13083a6b74b05d6c300f61cd8babb2b80
xiaohu <1205150638@qq.com>
2022-12-05 15:38:33 +0800

    first commit

--- nowcoder/doc/play-algorithms-and-data-structures/11-QuickSort/03-QuickSort/src/QuickSort.java
+++ nowcoder/doc/play-algorithms-and-data-structures/11-QuickSort/03-QuickSort/src/QuickSort.java
@@ -20,13 +20,14 @@ public class QuickSort {
     private static <E extends Comparable<E>> int partition(E[] arr, int l, int r){
 
         // arr[l+1...j] < v ; arr[j+1...i] >= v
+        //j作为分界索引，小于j 索引对应的值 小于v 大于J 索引对应的值 大于v
         int j = l;
-        for(int i = l + 1; i <= r; i ++)
+        for(int i = l + 1; i <= r; i ++){
             if(arr[i].compareTo(arr[l]) < 0){
                 j ++;
                 swap(arr, i, j);
             }
-
+        }
         swap(arr, l, j);
         return j;
     }

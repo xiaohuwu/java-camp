a40ab823407825dc4e676939740d2942d1d532d3
xiaohu <1205150638@qq.com>
2023-02-22 17:24:19 +0800

    first commit

--- spring/spring-2023/src/test/java/SpringAnnotaionTest.java
+++ spring/spring-2023/src/test/java/SpringAnnotaionTest.java
@@ -2,12 +2,10 @@ import org.example.MainConfig;
 import org.example.model.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import java.util.Arrays;
 import java.util.Map;
 

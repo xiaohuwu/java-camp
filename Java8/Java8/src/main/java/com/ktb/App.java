6611e02a7528362109abff79f0c2625cc9a1f61e
xiaohu <1205150638@qq.com>
2023-03-16 17:31:57 +0800

    first commit

--- Java8/Java8/src/main/java/com/ktb/App.java
+++ Java8/Java8/src/main/java/com/ktb/App.java
@@ -1,13 +1,56 @@
 package com.ktb;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String args[]) throws IOException {
+
+        // Setting up collection class
+        final int MAX_ORDERS = 50000000; // 50 mn integers
+        List<Integer> values = new ArrayList<>(MAX_ORDERS);
+        Random rand = new Random(System.nanoTime());
+        for (int i = 0; i < MAX_ORDERS; i++) {
+            Integer number = rand.nextInt();
+            values.add(number);
+        }
+
+        // Starting sequential filtering
+        long startTime = System.nanoTime();
+
+        long count = values.stream().filter(i -> i % 31 == 0).count();
+        // long count = values.stream().sorted().count();
+        //uncomment to test sorting
+        System.out.println(count);
+
+        long endTime = System.nanoTime();
+
+        long timeTakenInMillis = TimeUnit.NANOSECONDS
+                .toMillis(endTime - startTime);
+        System.out.println(String.format("Sequential filtering took: %d ms",
+                timeTakenInMillis));
+
+
+        // Starting parallel filtering of Collection
+        long start = System.nanoTime();
+
+        long count1 = values.parallelStream().filter(i -> i % 31 == 0).count();
+        // long count1 = values.parallelStream().sorted().count();
+        //uncomment to test sorting
+        System.out.println(count1);
+
+        long end = System.nanoTime();
+
+        long elapsed = TimeUnit.NANOSECONDS.toMillis(end - start);
+        System.out.println(String.format("parallel filtering took: %d ms",
+                elapsed));
+
     }
 }
--- sql/practice_01.sql
+++ sql/practice_01.sql
@@ -1,9 +1,45 @@
 --  以下题目均采集于 https://zhuanlan.zhihu.com/p/43289968
 
 -- 1.查询课程编号为“01”的课程比“02”的课程成绩高的所有学生的学号
+select Student.*, a.s_score, b.s_score
+from Student
+         inner join
+         (select * from Score where c_id = '02') as a on a.s_id = Student.s_id
+         inner join
+         (select * from Score where c_id = '01') as b on a.s_id = b.s_id
+where a.s_score > b.s_score;
+
+
+-- 查询平均成绩大于60分的学生的学号和平均成绩 类似的题目
+select Score.s_id, avg(Score.s_score)
+from Score
+group by s_id
+having avg(Score.s_score) > 60;
+
+--  查询所有学生的学号、姓名、选课数、总成绩
+
+select S.s_id,
+       S.s_name,
+       count(*) AS course_num,
+       sum(s_score)
+from Score
+         inner join Student S on Score.s_id = S.s_id
+group by Score.s_id;
+
+
+SELECT s.s_id,
+       s.s_name,
+       COUNT(sc.c_id)  AS course_num,
+       SUM(sc.s_score) AS total_score
+FROM Student s
+         LEFT JOIN Score sc ON s.s_id = sc.s_id
+GROUP BY s.s_id,
+         s.s_name;
 
 
 -- 查询没学过“张三”老师课的学生的学号、姓名
+
+
 select *
 from Student
 where s_id not in (select distinct Score.s_id
@@ -12,7 +48,51 @@ where s_id not in (select distinct Score.s_id
                             inner join Teacher T on C.t_id = T.t_id
                    where T.t_name = '张三');
 
+
+
+SELECT s.s_id, s.s_name
+FROM Student s
+WHERE NOT EXISTS(
+        SELECT *
+        FROM Course c
+                 INNER JOIN Teacher t ON c.t_id = t.t_id
+                 INNER JOIN Score sc ON c.c_id = sc.c_id
+        WHERE s.s_id = sc.s_id
+          AND t.t_name = '张三'
+    );
+
 -- 查询学过“张三”老师所教的所有课的同学的学号、姓名
+
+
+SELECT s.s_id, s.s_name
+FROM Student s
+WHERE NOT EXISTS(
+        SELECT c.c_id
+        FROM Course c
+                 JOIN Teacher t ON c.t_id = t.t_id
+        WHERE t.t_name = '张三'
+          AND c.c_id NOT IN (
+            SELECT c2.c_id
+            FROM Course c2
+                     JOIN Score sc ON c2.c_id = sc.c_id
+            WHERE sc.s_id = '01'
+        )
+    );
+
+
+
+SELECT c.c_id
+FROM Course c
+         JOIN Teacher t ON c.t_id = t.t_id
+WHERE t.t_name = '张三'
+  AND c.c_id NOT IN (
+    SELECT c2.c_id
+    FROM Course c2
+             JOIN Score sc ON c2.c_id = sc.c_id
+    WHERE sc.s_id = '02'
+);
+
+
 select *
 from Student
 where s_id in (select distinct Score.s_id
@@ -22,32 +102,100 @@ where s_id in (select distinct Score.s_id
                where T.t_name = '张三');
 
 
+select C.c_id
+from Course C
+         inner join Teacher T on C.t_id = T.t_id
+where T.t_name = '张三';
+
 -- 7、查询学过编号为“01”的课程并且也学过编号为“02”的课程的学生的学号、姓名
 
-select *
-from Student
-where s_id in (select a.s_id
-               from (select * from Score where c_id = "01") as a
-                        inner join
-                        (select * from Score where c_id = "02") as b on a.s_id = b.s_id)
+
+select a.s_id, Student.s_name
+from (select * from Score where c_id = '01') as a
+         inner join (select * from Score where c_id = '02') as b
+                    on a.s_id = b.s_id
+         inner join Student
+                    on Student.s_id = a.s_id;
+
+
+
+SELECT s.s_id, s.s_name
+FROM Student s
+         INNER JOIN Score sc1 ON s.s_id = sc1.s_id
+         INNER JOIN Score sc2 ON s.s_id = sc2.s_id
+WHERE sc1.c_id = '01'
+  AND sc2.c_id = '02';
+
+-- 8、查询课程编号为“02”的总成绩（不重点）
+
+select sum(Score.s_score)
+from Score
+where c_id = '02';
+
+
+-- 9、查询所有课程成绩小于60分的学生的学号、姓名
+select * from Score where s_id = '08';
+select * from Student where s_id not  in (select s_id
+                                          from Score where s_score >= 60);
+
+
+-- 10.查询没有学全所有课的学生的学号、姓名(重点)
+
+
 
 -- 10.查询没有学全所有课的学生的学号、姓名
-
 select Student.*
-from Student
-         inner join
-     (select s_id, count(*) from Score group by s_id having count(*) < (select count(distinct c_id) from Course)) as b
+from  (select s_id, count(*) from Score group by s_id having count(*) < (select count(distinct c_id) from Course)) as b
+inner join Student
+on Student.s_id = b.s_id
 where Student.s_id = b.s_id;
+
+-- 11、查询至少有一门课与学号为“01”的学生所学课程相同的学生的学号和姓名（重点）
+select  Student.s_id,Student.s_name from (select distinct s_id from  Score where c_id in (select c_id from Score where s_id = '01')) as s
+    inner join  Student
+    on Student.s_id = s.s_id;
+
+
+
+
 
 -- 查询至少有一门课与学号为“01”的学生所学课程相同的学生的学号和姓名
 
 select Student.*
 from Student
          inner join
-     (select distinct s_id from Score  where Score.c_id in (select c_id from Score where s_id= '01') ) as b
+     (select distinct s_id from Score where Score.c_id in (select c_id from Score where s_id = '01')) as b
 where Student.s_id = b.s_id;
 
 -- 12.查询和“01”号同学所学课程完全相同的其他同学的学号(重点)
-select * from
-            (select s_id,c_id from Score where s_id= '01') as a
-    inner join Score on Score.s_id = a.s_id
+select *
+from (select s_id, c_id from Score where s_id = '01') as a
+         inner join Score on Score.s_id = a.s_id;
+
+-- 12.查询和“01”号同学所学课程完全相同的其他同学的学号(重点)
+
+select s_id from score
+where s_id != '01'
+  and s_id not in (
+    select s_id from score where c_id not in ( select c_id from score where s_id = '01')
+      )
+group by s_id
+having count(*) = (select count(*) from score where s_id = '01')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

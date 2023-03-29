d3dd7adb882b9ce2b2284c39deb95deebc3ac496
xiaohu <1205150638@qq.com>
2023-03-30 00:42:09 +0800

    first commit

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,8 @@
+drop database demo;
+create database  demo;
+use demo;
+
+
 
 -- 建表
 -- 学生表
@@ -69,31 +74,133 @@ insert into Score values('07' , '02' , 89);
 insert into Score values('07' , '03' , 98);
 
 
+-- 1.查询课程编号为“01”的课程比“02”的课程成绩高的所有学生的学号
 
-INSERT INTO t1(id, pattern)
-VALUES(1,'Divot'),
-      (2,'Brick'),
-      (3,'Grid');
-
-INSERT INTO t2(id, pattern)
-VALUES('A','Brick'),
-      ('B','Grid'),
-      ('C','Diamond');
-
-select * from t1;
-select * from t2;
+select Student.*,a.s_score,b.s_score from
+(select * from Score where c_id = '01') as a
+inner join
+(select * from Score where c_id = '02') as b
+on a.s_id = b.s_id
+inner join Student on Student.s_id = a.s_id
+where a.s_score > b.s_score;
 
 
-select * from Student;
+-- 2、查询平均成绩大于60分的学生的学号和平均成绩
+select s_id,avg(s_score) from Score
+group by s_id
+having avg(s_score) > 60;
 
-show create table student;
+-- 3、查询所有学生的学号、姓名、选课数、总成绩（不重要）
+
+select S.s_id,S.s_name,count(*),sum(Score.s_score)
+from Score
+inner join Student S on Score.s_id = S.s_id
+group by S.s_id, S.s_name;
+
+-- 4、查询姓“猴”的老师的个数（不重要）
+
+select count(*)
+from Teacher
+where t_name like '猴%';
+
+-- 5、查询没学过“张三”老师课的学生的学号、姓名（重点）
+select * from Student
+where s_id not in (
+    select distinct Score.s_id  from Score
+    inner join Course C on Score.c_id = C.c_id
+    inner join Teacher T on C.t_id = T.t_id
+    where T.t_name = '张三'
+);
+
+-- 6、查询学过“张三”老师所教的所有课的同学的学号、姓名（重点）
+
+SELECT C.s_id
+FROM (
+         SELECT s.s_id,s.c_id FROM score s
+         WHERE s.c_id IN(
+             SELECT c_id FROM course c
+             INNER JOIN teacher t ON c.t_id=t.t_id
+             WHERE t_name ='张三')
+     ) C
+GROUP BY C.s_id
+HAVING COUNT(*) = (
+    SELECT COUNT(*)
+    FROM Course c
+             INNER JOIN Teacher t
+                        ON c.t_id = t.t_id
+    WHERE t.t_name ='张三'
+);
+
+-- 7、查询学过编号为“01”的课程并且也学过编号为“02”的课程的学生的学号、姓名（重点）
+
+select * from
+(select * from Score where c_id = '01') as a
+inner join
+(select * from Score  where c_id = '02') as b
+on a.s_id = b.s_id
+inner join Student on Student.s_id = a.s_id;
 
 
-desc student;
+
+  select s_id from
+  (  (select * from Score where c_id = '01')
+     union all
+     (select * from Score  where c_id = '02')) b
+ group by s_id having count(*) =2
 
 
 
-ALTER TABLE student modify s_birth VARCHAR(20)  comment '这是一个注释';
+
+-- 8、查询课程编号为“02”的总成绩
+
+select sum(s_score) from
+Score
+where Score.c_id = '02';
+
+-- 9、查询所有课程成绩小于60分的学生的学号、姓名
+
+select s_id,max(s_score) as max_score from Score
+group by s_id
+having max(s_score)< 60;
 
 
-SHOW CREATE TABLE Student;
+select Score.s_id from Score
+where Score.s_score <  60
+group by Score.s_id
+having count(*) =(select count(distinct Course.c_id) from Course)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# INSERT INTO t1(id, pattern)
+# VALUES(1,'Divot'),
+#       (2,'Brick'),
+#       (3,'Grid');
+#
+# INSERT INTO t2(id, pattern)
+# VALUES('A','Brick'),
+#       ('B','Grid'),
+#       ('C','Diamond');
+#
+# select * from t1;
+# select * from t2;
